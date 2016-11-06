4e940be92efd18483050295c155a65f16fa6bab7
Jeremy Castagno <Jeremy Castagno>
2016-11-06 18:46:46 -0500

    Initial Broken

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <div>
+      <grid rows='10' cols='10'>
+
+      </grid>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Grid from './Grid.vue'
+export default {
+  name: 'app',
+  components: {
+    Grid
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/Grid.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+    <table class="grid" @mousedown="startDrag" @touchstart="startDrag" @mousemove="onDrag" @touchmove="onDrag"
+    @mouseup="stopDrag" @touchend="stopDrag" @mouseleave="stopDrag">
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'grid',
+  props: {
+    rows: 'Number',
+    cols: 'Number',
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      dragging: false,
+    }
+  },
+  methods: {
+    startDrag: function (e) {
+      e = e.changedTouches ? e.changedTouches[0] : e
+      this.dragging = true
+      console.log(e.x, e.y)
+      },
+    onDrag: function (e) {
+      e = e.changedTouches ? e.changedTouches[0] : e
+      if (this.dragging) {
+        console.log(e.x, e.y)
+      }
+    },
+    stopDrag: function () {
+      if (this.dragging) {
+        this.dragging = false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.grid { margin:1em auto; border-collapse:collapse }
+.grid td {
+    cursor:pointer;
+    width:30px; height:30px;
+    border:1px solid #ccc;
+    text-align:center;
+    font-family:sans-serif; font-size:13px
+}
+.grid td.clicked {
+    background-color:yellow;
+    font-weight:bold; color:red;
+}
+</style>
